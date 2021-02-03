a1afbb6fc6159931d367ce36b960d5d7ee70f22c
antimage-stack <vanua.osipovporg@gmail.com>
2021-02-03 20:17:41 +0300

    feat: my script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( !array || array.length == 0 ) return 0
+
+  array.sort( (a,b) => a - b)
+  return array[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( !array || array.length == 0 ) return 0
+
+  array.sort( (a,b) => b - a)
+  return array[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if ( !array || array.length == 0 ) return 0
+
+  let avg = 0
+  for ( let num of array ) {
+    avg += num
+  }
+  return avg / array.length    
+} 
+
